72511c58da8ab61eab809d03e93081ad742d5087
Vettri vaishali.s <csvettrivaishali@gmail.com>
2025-12-16 21:31:40 +0530

    Add files via upload

---
+++ Java.Codes/C80_MCE/src/com/tnsif/basic/java/FirstProgram.java
@@ -0,0 +1,9 @@
+//Demonstration of simple java program
+package com.tnsif.basic.java;
+import java.util.LinkedList ; //all subpackages
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
