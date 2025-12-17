1fbc9ae6cb04ac0a1f483fa666ef0afd46a45eb7
Vettri vaishali.s <csvettrivaishali@gmail.com>
2025-12-17 09:50:32 +0530

    Delete Java.Codes/C80_MCE/src/com/tnsif/basic/java directory

--- Java.Codes/C80_MCE/src/com/tnsif/basic/java/FirstProgram.java
+++
@@ -1,9 +0,0 @@
-//Demonstration of simple java program
-package com.tnsif.basic.java;
-import java.util.LinkedList ; //all subpackages
-
-public class FirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
